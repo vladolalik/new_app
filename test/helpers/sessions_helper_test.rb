1a24e7f10c44c0e041b92a5029212f16b4ada180
vladolalik <wladolalik@gmail.com>
2014-10-11 02:26:52 +0200

    Finish log in/log out

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SessionsHelperTest < ActionView::TestCase
+
+  test "current_user" do
+    user = users(:michael)
+    remember(user)
+    assert_equal user, current_user
+  end
+
+end
